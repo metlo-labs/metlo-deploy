495f9d2acd70e93c0c6b0aea8a8a24d156f91c25
Ninad Sinha <ninad@metlo.com>
2022-11-26 22:06:07 +0530

    Remove  python3.6

--- azure/setup_python.sh
+++ azure/setup_python.sh
@@ -1,6 +1,7 @@
 sudo apt-get update
 sudo apt-get install software-properties-common -y
 sudo add-apt-repository ppa:deadsnakes/ppa -y
+sudo apt-get remove python3.6
 sudo apt-get update
 sudo apt-get install python3.8 -y
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 1

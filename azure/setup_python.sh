fe052fa88df0ec65d9704532c2e130789a14828a
Ninad Sinha <ninad@metlo.com>
2022-11-26 21:21:26 +0530

    Remove and reinstall apt-pkg

--- azure/setup_python.sh
+++ azure/setup_python.sh
@@ -4,3 +4,5 @@ sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt-get update
 sudo apt-get install python3.8 -y
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 1
+sudo apt-get remove --purge apt_pkg
+sudo apt-get install python3-apt -y

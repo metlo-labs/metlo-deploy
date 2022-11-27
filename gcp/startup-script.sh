4ce562d85bfe1d6eca186a4cea4a3f779bbb49df
Akshay Shekhawat <akshay.shekhawat@gmail.com>
2022-11-26 16:29:15 -0800

    Update startup-script.sh

--- gcp/startup-script.sh
+++ gcp/startup-script.sh
@@ -1,3 +1,14 @@
+#!/usr/bin/env bash
+
+command_present() {
+  type "$1" >/dev/null 2>&1
+}
+
+if command_present metlo-deploy; then
+  echo "metlo-deploy command already exists..."
+  exit 0
+fi
+
 export LICENSE_KEY=<YOUR_LICENSE_KEY>
 wget https://raw.githubusercontent.com/metlo-labs/metlo-deploy/main/deploy.sh
 chmod +x deploy.sh

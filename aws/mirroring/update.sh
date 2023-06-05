01b47f6cb68b802bfc0a166ebb3d86cb341abe0d
Akshay Shekhawat <akshay.shekhawat@gmail.com>
2023-06-04 20:39:46 -0700

    Update update.sh

--- aws/mirroring/update.sh
+++ aws/mirroring/update.sh
@@ -3,7 +3,7 @@
 export WHOAMI=metlo
 
 echo "UPDATING"
-sudo curl -L https://metlo-releases.s3.us-west-2.amazonaws.com/metlo_agent_linux_amd64_latest > /usr/local/bin/metlo-agent
+sudo curl -L https://metlo-releases.s3.us-west-2.amazonaws.com/metlo_agent_pcap_linux_amd64_latest > /usr/local/bin/metlo-agent
 sudo curl -L https://raw.githubusercontent.com/metlo-labs/metlo-deploy/main/aws/mirroring/metlo-traffic-mirror.service > /home/$WHOAMI/metlo/metlo-traffic-mirror.service
 sudo cp /home/$WHOAMI/metlo/metlo-agent /usr/local/bin
 sudo mv /home/$WHOAMI/metlo/metlo-traffic-mirror.service /lib/systemd/system/metlo-traffic-mirror.service -f

7df754f69d39b8c2345638ab798f133c190f507d
Akshay Shekhawat <akshay.shekhawat@gmail.com>
2023-06-04 20:38:48 -0700

    Update install.sh

--- aws/mirroring/install.sh
+++ aws/mirroring/install.sh
@@ -8,7 +8,7 @@ export WHOAMI=metlo
 
 echo "GETTING FILES"
 mkdir -p /home/$WHOAMI/metlo
-sudo curl -L https://metlo-releases.s3.us-west-2.amazonaws.com/metlo_agent_linux_amd64_latest > /usr/local/bin/metlo-agent
+sudo curl -L https://metlo-releases.s3.us-west-2.amazonaws.com/metlo_agent_pcap_linux_amd64_latest > /usr/local/bin/metlo-agent
 sudo curl -L https://raw.githubusercontent.com/metlo-labs/metlo-deploy/main/aws/mirroring/metlo-traffic-mirror.service > /home/$WHOAMI/metlo/metlo-traffic-mirror.service
 sudo chmod +x /usr/local/bin/metlo-agent
 
